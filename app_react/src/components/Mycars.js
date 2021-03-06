80db937f6a9597a186ded13041e11f402558d18e
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-03-06 12:09:35 +0400

    Ajout DOM events et changement de State

--- app_react/src/components/Mycars.js
+++ app_react/src/components/Mycars.js
@@ -3,15 +3,25 @@ import Car from './Cars';
 
 //On crée la classe Mycars
 class Mycars extends Component {
+
+    //Fonction qui va se lancer quand quelqu'un va copier le texte
+    noCopy = () => {
+        alert('merci de ne pas copier le texte');
+    };
+
+    // //On va ajouter un style lors du survol du titre h1
+    // addStyle = (e) => {
+    //     e.target.classList.toggle('styled');        //toggle si la classe existe la supprime, sinon la rajoute
+    // };
+
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
-
+                <p onCopy={this.noCopy}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 <Car color="red"> Ford </Car>
                 <Car > Mercedes </Car>
                 <Car color="blue"></Car>
-             
             </div>)
     }
 }
