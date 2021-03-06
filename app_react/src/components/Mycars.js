28360c1e736b668f77b2b285892cde07e96e38c5
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-03-06 10:55:54 +0400

    Création d'un component de type class et l'importer dans la div parent

---
+++ app_react/src/components/Mycars.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';   //On importe react de react
+
+//On crée la classe Mycars
+class Mycars extends Component {    
+    render(){
+        return <h1>Hello React</h1>
+    }
+}
+
+//Puis on exporte la classe pour pouvoir l'importer et l'afficher
+export default Mycars;
