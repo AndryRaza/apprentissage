2c33e5c62cf218269d70c1105c957e5c7a148341
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2021-03-17 17:10:34 +0400

    Ajout fonctionnalités paniers

--- tp_interface_vue/assets/js/vue.js
+++ tp_interface_vue/assets/js/vue.js
@@ -20,29 +20,76 @@ const Home = {
         return {
             products,
             searchKey: '',
-            liked: []
+            liked: [],
+            cart: []
         };
     },
-    computed:{
+    computed: {
         //Fonction de recherche, 
-        filteredList(){
-            return this.products.filter((product)=>{
+        filteredList() {
+            return this.products.filter((product) => {
                 return product.description.toLowerCase().includes(this.searchKey.toLowerCase());
             });
         },
-        getLikeCookie(){
+        getLikeCookie() {
             let cookieValue = JSON.parse($cookies.get('like'));
             cookieValue == null ? this.liked = [] : this.liked = cookieValue;
+        },
+        cartTotalAmount() {
+            let total = 0;
+            for (let item in this.cart) {
+                total = total + (this.cart[item].quantity * this.cart[item].price);
+            }
+            return total;
+        },
+        itemTotalAmount() {
+            let itemTotal = 0;
+            for (let item in this.cart) {
+                itemTotal = itemTotal + (this.cart[item].quantity);
+            }
+            return itemTotal;
         }
+
     },
-    methods:{
-        setLikeCookie(){
-            document.addEventListener('input',()=>{
-                setTimeout(()=>{
-                    $cookies.set('like',JSON.stringify(this.liked)); //fonction donné par plug in vue cookie
-                },300);
-               
+    methods: {
+        setLikeCookie() {
+            document.addEventListener('input', () => {
+                setTimeout(() => {
+                    $cookies.set('like', JSON.stringify(this.liked)); //fonction donné par plug in vue cookie
+                }, 300);
+
             });
+        },
+        addToCart(product) {
+            //check if already if array
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i].id === product.id) {
+                    return this.cart[i].quantity++;
+                }
+            };
+
+            this.cart.push({
+                id: product.id,
+                img: product.img,
+                description: product.description,
+                price: product.price,
+                quantity: 1
+            });
+        },
+        cartPlusOne(product) {
+            product.quantity = product.quantity + 1;
+        },
+        cartRemoveItem(id) {
+            this.$delete(this.cart, id);     //Pour enlever un élément du DOM, c'est du vu $delete
+        },
+        cartMinusOne(product, id) {
+
+            if (product.quantity == 1) {
+                this.cartRemoveItem(id);
+            }
+
+            product.quantity = product.quantity - 1;
+
         }
     },
     mounted: () => {
